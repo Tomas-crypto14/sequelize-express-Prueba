14103b9ff003a2f3798a62ef4d94a8c4308398f1
Tomas-crypto14 <tomasignaciocapalbo@gmail.com>
2025-04-29 12:37:52 +0200

    Sequelize actualizado

--- server/models/Book.js
+++ server/models/Book.js
@@ -1,6 +1,6 @@
 const { DataTypes } = require("sequelize");
 const db = require("../db");
-
+//El nombre de la const que lee Book debe ser la misma que la ruta del module.exports
 
   const Book = db.sequelize.define(
     "Book",
