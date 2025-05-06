72049ba63e870c6a81e0af55dc05342e8363370c
Tomas-crypto14 <tomasignaciocapalbo@gmail.com>
2025-05-06 08:18:12 +0200

    Actualizado

--- server/models/Book.js
+++ server/models/Book.js
@@ -1,5 +1,4 @@
 const { DataTypes } = require("sequelize");
-<<<<<<< HEAD
 const db = require("../db");
 //El nombre de la const que lee Book debe ser la misma que la ruta del module.exports
 
@@ -19,25 +18,4 @@ const db = require("../db");
     },
     {}
   );
-=======
-const db = require("../db")
-
-const Book = db.sequelize.define(
-  "Book",
-  {
-    title: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    description: {
-      type: DataTypes.STRING,
-    },
-    length: {
-      type: DataTypes.INTEGER,
-    },
-  },
-  {}
-);
-
->>>>>>> 8199d50dddc6a3c1a31790d1dafd93e68afea4b9
 module.exports = Book;
